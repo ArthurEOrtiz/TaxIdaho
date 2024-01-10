f27df9cc5e11ac71113b13d933860b3dfe5d7d98
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-01-10 08:19:42 -0700

    Clean Code

--- TaxIdaho/Models/SchoolInfo.cs
+++ TaxIdaho/Models/SchoolInfo.cs
@@ -3,12 +3,12 @@
 	public class SchoolInfo
 	{
 		public int Id { get; set; }
-		public string SSchoolType { get; set; } = "Something";
+		public string SSchoolType { get; set; } = String.Empty;
 		public DateTime SDateSchool { get; set; }
 		public int SSeq { get; set; }
-		public string? SCity { get; set; } = "Something";
-		public string? SLocation1 { get; set; } = "Something";
-		public string? SLocation2 { get; set; } = "Something";
+		public string? SCity { get; set; } 
+		public string? SLocation1 { get; set; }
+		public string? SLocation2 { get; set; } 
 		public DateTime SDeadLine { get; set; } 
 	}
 }
